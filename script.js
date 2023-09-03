7b0a7ab69cb1687eb7ec314201e172167b94a3ab
александр <85447129+Alexander-Guzhvin@users.noreply.github.com>
2023-09-03 19:25:17 +0300

    new

--- script.js
+++ script.js
@@ -14,3 +14,90 @@ const swiper = new Swiper('.swiper', {
     },
 
   });
+
+
+
+
+
+
+  const filterContainer = document.querySelector(".gallery-filter"),
+  galleryItems = document.querySelectorAll(".gallery-item");
+ 
+  filterContainer.addEventListener("click", (event) =>{
+    if(event.target.classList.contains("filter-item")){
+       // deactivate existing active 'filter-item'
+       filterContainer.querySelector(".active").classList.remove("active");
+       // activate new 'filter-item'
+       event.target.classList.add("active");
+       const filterValue = event.target.getAttribute("data-filter");
+       galleryItems.forEach((item) =>{
+        if(item.classList.contains(filterValue) || filterValue === 'all'){
+          item.classList.remove("hide");
+           item.classList.add("show");
+        }
+        else{
+          item.classList.remove("show");
+          item.classList.add("hide");
+        }
+       });
+    }
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  var lastId,
+    topMenu = $("#top-menu"),
+    topMenuHeight = topMenu.outerHeight() - 2,
+    // All list items
+    menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function(){
+      var item = $($(this).attr("href"));
+      if (item.length) { return item; }
+    });
+
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+menuItems.click(function(e){
+  var href = $(this).attr("href"),
+      offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+  $('html, body').stop().animate({ 
+      scrollTop: offsetTop
+  }, 300);
+  e.preventDefault();
+});
+
+// Bind to scroll
+$(window).scroll(function(){
+   // Get container scroll position
+   var fromTop = $(this).scrollTop()+topMenuHeight;
+   
+   // Get id of current scroll item
+   var cur = scrollItems.map(function(){
+     if ($(this).offset().top < fromTop)
+       return this;
+   });
+   // Get the id of the current element
+   cur = cur[cur.length-1];
+   var id = cur && cur.length ? cur[0].id : "";
+   
+   if (lastId !== id) {
+       lastId = id;
+       // Set/remove active class
+       menuItems
+         .parent().removeClass("active")
+         .end().filter("[href='#"+id+"']").parent().addClass("active");
+   }                   
+});
